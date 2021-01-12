0a7571de2bef4cc9a70df2863070a81580049191
gCass <guglielmo.cassini01@universitadipavia.it>
2021-01-12 21:19:16 +0100

    Fatto codice per pulire il fasta e creato il file cleared_fasta.txt

--- python/pulire_fasta.py
+++ python/pulire_fasta.py
@@ -1,9 +1,15 @@
 path = "../dataset/cog_2020-04-24_alignment.fasta"
 fread = open(path,"r")
+basi_azotate = ["A","T","C", "G"]
+cleared_txt = ""
 for i in range(1,10):
     txt = fread.read(500)
-    print(txt)
+    cleared_txt = cleared_txt +  ''.join([i for i in txt if i in basi_azotate])
 fread.close()
     
+path = "../dataset/cleared_fasta.txt"
+fw = open(path,"w")
+fw.write(cleared_txt)
+fw.close()
 
 

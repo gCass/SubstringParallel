0629903ef47b7aab28a82ca1dbf20dbf09c013bf
gCass <guglielmo.cassini01@universitadipavia.it>
2021-01-20 12:50:26 +0100

    Aggiornato il fasta che pulisce tutto, dura più o meno un'ora

--- python/pulire_fasta.py
+++ python/pulire_fasta.py
@@ -2,8 +2,10 @@ path = "../dataset/cog_2020-04-24_alignment.fasta"
 fread = open(path,"r")
 basi_azotate = ["A","T","C", "G"]
 cleared_txt = ""
-for i in range(1,10):
+while True:
     txt = fread.read(500)
+    if not txt:
+        break
     cleared_txt = cleared_txt +  ''.join([i for i in txt if i in basi_azotate])
 fread.close()
     

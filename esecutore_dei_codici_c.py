c31c3d283553b364fa482d883a4fa689625d9bec
gCass <guglielmo.cassini01@universitadipavia.it>
2021-02-11 23:16:47 +0100

    Finita una prima bozza del codice, discutere il fatto che potremmo limitare dinamicamente la vm da usare un certo numero di thread deciso da noi per automatizzare il tutto da python

--- esecutore_dei_codici_c.py
+++ esecutore_dei_codici_c.py
@@ -27,7 +27,50 @@ def exec_blocchi(path1="dataset/stringa_lcs_1.txt",path2="dataset/stringa_lcs_2.
     t_lcs = l[3]
     return t_lett, t_lcs
 
-#Codice
+
+#Prove
+#t_lett, t_lcs = exec_blocchi()
+#print(str(t_lett) + " " + str(t_lcs) )
+
+
+
+#Codice vero
+#Inizializzazione
 df = pd.DataFrame()
-t_lett, t_lcs = exec_blocchi()
-print(str(t_lett) + " " + str(t_lcs) )
+block_sizes = [50,100,200,300,400,500]
+Ns_thread = [4] 
+column_names = ["N_thread","File1","File1_size","File2","File2_size","t_serial_lett","t_serial_lcs","block_size","t_paral_lett","t_paral_lcs"]
+df = pd.DataFrame(columns = column_names)
+print(df)
+
+#Inizio cicli
+
+file1 = "dataset/stringa_lcs_1.txt"
+file1_size = 10000
+file2 = "dataset/stringa_lcs_2.txt"
+file2_size = 10000
+#Ci vorrebbe un ciclo ancora più esterno che fa variare i file, ma quello dopo
+for n_thread in Ns_thread:
+    #Eseguo il seriale una volta sola dato il numero di thread
+    t_lett_serial,t_lcs_serial = exec_base()
+    for block in block_sizes:
+        t_lett_parallel,t_lcs_parallel = exec_blocchi(block=block)
+        df = df.append({"N_thread":n_thread, "File1": file1 ,"File1_size": file1_size ,
+                        "File2": file2,"File2_size":file2_size,
+                        "t_serial_lett": t_lett_serial,"t_serial_lcs": t_lcs_serial,
+                        "block_size":block,"t_paral_lett":t_lett_parallel,"t_paral_lcs":t_lcs_parallel}
+                       , ignore_index=True)
+
+
+from tabulate import tabulate
+print(tabulate(df, headers='keys', tablefmt='simple'))
+df.to_csv("risultati_cicli.csv", index=False)
+
+        
+        
+
+
+
+
+
+        
